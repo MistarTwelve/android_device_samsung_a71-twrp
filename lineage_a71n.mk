f6036fec6c1f3e65233d80d53252984bcb0af4e8
Faiz Authar <faizauthar@gmail.com>
2020-05-29 11:09:31 +0700

    a71n: initial TWRP tree

---
+++ lineage_a71n.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := a71n
+
+DEVICE_PATH := device/samsung/a71n
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a71n
+PRODUCT_NAME := lineage_a71n
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A715F
+PRODUCT_MANUFACTURER := samsung
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-3
