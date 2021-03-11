5d9f8b2f85b1d2ac5e27c4a856fe17aa23a3d6ee
Faiz Authar <faizauthar@gmail.com>
2021-03-11 18:19:48 +0700

    a71: device: Enable Dynamic partitions flags

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@
 
 LOCAL_PATH := device/samsung/a71
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
